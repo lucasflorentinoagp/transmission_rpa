7e93fb45e1e7851656f6460a9a2bd2d270d70787
lucasflorentinoagp <lucasflorentino@grpereira.com.br>
2025-03-03 10:02:12 -0400

    change lib rpa to pyautogui

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import rpa as r
+import pyautogui as r
 
 def main():
-    # mouse move to 100, 100
-    r.init(visual_automation=True)
-    r.mouse_move(100, 100)
-    r.mouse_down()
-    r.mouse_up()
+    # mouse move to x=100, y=100
+    r.moveTo(100, 100)
+    # mouse move to x=200, y=200 in 2 seconds
+    r.moveTo(200, 200, 2)
+    # mouse move to x=300, y=300 in 2 seconds with linear movement
+    r.moveTo(300, 300, 2, r.easeInOutQuad)
+    # mouse move to x=400, y=400 in 2 seconds with linear movement
+    r.moveTo(400, 400, 2, r.easeInOutQuad)
 
 
 if __name__ == "__main__":
